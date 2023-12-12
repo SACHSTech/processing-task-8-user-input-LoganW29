caa2e914cbf32a8e52a15ce76e7a60949d76fd1e
LoganW29 <Lw1103724>
2023-12-12 17:59:42 -0500

    added methods

--- Sketch.java
+++ Sketch.java
@@ -16,30 +16,107 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+    background(12, 122, 232);
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
-  public void draw() {
+  
+   public void draw() {
+    if (drawClouds == true) {
+      for (int i = 10; i < width; i += 90){
+      drawCloud(i, 40);
+      }
+    }
+    
+  }
+
+  public void keyReleased() {
+    if (keyCode == UP) {
+      drawClouds = false;
+    }
+  }
+  
+  public void mousePressed() {
+    drawFlower(mouseX, mouseY);
+  }
+
+  public void keyPressed() {
+
+    // Change background colour if the 'B' or 'b' key is pressed 
+    if (key == 'B' || key == 'b') {
+    changeBackground(28, 52, 115);
+    }
+    // if the up key is pressed it draws a cloud where the mouse cursor is
+    if (keyCode == UP) {
+      drawClouds = true;
+    } 
+    
+  }   
+  /**
+   * Changes the background if a key is pressed 
+   * @param intR determines red 
+   * @param intG determines green 
+   * @param intB determines blue 
+   */
+  public void changeBackground(int intR, int intG, int intB) {
+    background(intR, intG, intB);
+  }
+
+  /**
+   * Calculations to draw flower
+   * @param intX
+   * @param intY
+   */
+  public void drawFlower(int intX, int intY) {
+    stroke(0);
+    fill(47, 145, 42);
+    rect(intX - 10, intY + 20, 10, 50);
+   
+    fill(232, 12, 188);
+    ellipse(intX + 10, intY + 30, 30, 30);
+    ellipse(intX - 20, intY + 30, 30, 30);
+    ellipse(intX + 10, intY, 30, 30);
+    ellipse(intX - 20, intY, 30, 30);
+   
+    fill(219, 76, 29);
+    ellipse (intX - 5, intY + 15, 30, 30);
 
   }
-  
+
   public void mouseDragged() {
-    drawGrass(mouseX, mouseY, 20, 20);
+    drawGrass(mouseX, mouseY);
+
   }
-  
+   
   /**
    * Draws grass where the mouse is dragged
    * @param intX
    * @param intY
-   * @param intWidth
-   * @param intHeight
    */
-  public void drawGrass(int intX, int intY, int intWidth, int intHeight) {
+  public void drawGrass(float fltX, float fltY) {
+    if (mouseY > 300){
+    noStroke();
     fill(66, 245, 96);
-    ellipse(intX, intY, intWidth, intHeight);
+    ellipse(fltX, fltY, 30, 30);
+    }
   }
+
+
+  boolean drawClouds = false;
+ 
+  public void drawCloud(int intX, int intY){
+    
+    if (keyCode == UP){
+    noStroke();
+    fill(255);
+    ellipse(intX + 5, intY + 15, 30, 30);
+    ellipse(intX - 10, intY + 15, 30, 30);
+    ellipse(intX + 10, intY, 30, 30);
+    ellipse(intX - 5, intY, 30, 30);
+    }
+  }
+
 
 }

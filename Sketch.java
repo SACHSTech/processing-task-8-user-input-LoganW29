4541c531ccfcdbcc6546196298fcda20755cbb14
LoganW29 <Lw1103724>
2023-12-13 09:36:05 -0500

    added methods

--- Sketch.java
+++ Sketch.java
@@ -1,24 +1,26 @@
 import processing.core.PApplet;
 
+/**
+ * A program that draws a garden using mouse and keyboard input variables and event functions.
+ * @author: L. Wong  
+ */
 public class Sketch extends PApplet {
 	
-	
   /**
-   * Called once at the beginning of execution, put your size all in this method
+   * Called once at the beginning of execution, the size call 
    */
   public void settings() {
-	// put your size call here
+	// size call 
     size(400, 400);
   }
 
   /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
+   * Called once at the beginning of execution. Initial set up
+   * values, i.e background, stroke, fill etc.
    */
   public void setup() {
-    changeBackground(12, 122, 232);
+    background(12, 122, 232);
   }
-
 
   boolean blnLeftPressed = false; 
   boolean blnRightPressed = false; 
@@ -28,16 +30,31 @@ public class Sketch extends PApplet {
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
-  
    public void draw() {
-    
+        
+    if (blnLeftPressed) {
+      butterflyX--;
+    }
+  
+    if (blnRightPressed) {
+      butterflyX++;
+    }
+    drawButterfly(butterflyX, butterflyY);
+
     if (drawClouds == true) {
       for (int i = 10; i < width; i += 90){
-      drawCloud(i, 40);
+        drawCloud(i, 40);
       }
     } 
-    handleMovement();
-    drawButterfly(butterflyX, butterflyY);
+   
+    if (keyPressed && key != 'b' && key != 'B' && keyCode != UP && keyCode != LEFT && keyCode != RIGHT){
+      for(int i = 0; i < width; i += 30){
+        drawFence(i, 320);
+      }
+      for(int i = 360; i < height; i += 20){
+      drawFence(0, i);
+      }
+    }
   }
 
   public void keyReleased() {
@@ -47,48 +64,53 @@ public class Sketch extends PApplet {
 
     if (keyCode == LEFT) {
         blnLeftPressed = false;
-    } else if (keyCode == RIGHT) {
+    } 
+    else if (keyCode == RIGHT) {
         blnRightPressed = false;
     }
 
   }
   
+  /**
+   * If mouse is pressed a flower is drawn to the screen at the mouseX and mouseY position.
+   */
   public void mousePressed() {
     drawFlower(mouseX, mouseY);
-
   }
 
-
+  /**
+   * If the mouse is dragged and mouseY is greater than 300, then grass is drawn. 
+   */
   public void mouseDragged() {
     drawGrass(mouseX, mouseY);
   }
 
+  /**
+   * Changes the background colour if he 'B' or 'b' key is pressed. 
+   * Adds clouds if the UP key is pressed.
+   */
   public void keyPressed() {
     // Change background colour if the 'B' or 'b' key is pressed 
     if (key == 'B' || key == 'b') {
-    changeBackground(28, 52, 115);
+      background(28, 52, 115);
     }
-    // if the up key is pressed it draws a cloud where the mouse cursor is
+    // if the UP key is pressed it draws a row of clouds in the sky.
     if (keyCode == UP) {
       drawClouds = true;
     } 
-       if (keyCode == LEFT) {
+    if (keyCode == LEFT) {
       blnLeftPressed = true;
-    } else if (keyCode == RIGHT) {
+    } 
+    else if (keyCode == RIGHT) {
       blnRightPressed = true;
     }
   }   
 
-  public void handleMovement() {
-    if (blnLeftPressed) {
-      butterflyX--;
-    }
-  
-    if (blnRightPressed) {
-      butterflyX++;
-    }
-  }
-  
+  /**
+   * Calculates the shapes that make up the butter fly. 
+   * @param butterflyX2 the X value 
+   * @param butterflyY2 the Y value 
+   */
   public void drawButterfly(float butterflyX2, float butterflyY2) {
    
     stroke(0);
@@ -105,23 +127,10 @@ public class Sketch extends PApplet {
     ellipse(butterflyX2 - 7, butterflyY2 + 5, 10, 50);
   }
 
-
-
-
   /**
-   * Changes the background if a key is pressed 
-   * @param intR determines red 
-   * @param intG determines green 
-   * @param intB determines blue 
-   */
-  public void changeBackground(int intR, int intG, int intB) {
-    background(intR, intG, intB);
-  }
-
-  /**
-   * Calculations to draw flower
-   * @param intX
-   * @param intY
+   * Calculations to draw the flower.
+   * @param intX the X value 
+   * @param intY the Y value 
    */
   public void drawFlower(int intX, int intY) {
     stroke(0);
@@ -136,14 +145,12 @@ public class Sketch extends PApplet {
    
     fill(219, 76, 29);
     ellipse (intX - 5, intY + 15, 30, 30);
-
   }
 
-   
   /**
-   * Draws grass where the mouse is dragged
-   * @param intX
-   * @param intY
+   * Draws grass where the mouse is dragged if the mouse's Y position is greater than 300. 
+   * @param intX the X value 
+   * @param intY the Y value 
    */
   public void drawGrass(float fltX, float fltY) {
     if (mouseY > 300){
@@ -153,20 +160,27 @@ public class Sketch extends PApplet {
     }
   }
 
-
   boolean drawClouds = false;
- 
+
+  /**
+  * Calculations for the shapes that make up the cloud. 
+  * @param intX the X value 
+  * @param intY the Y value
+  */
   public void drawCloud(int intX, int intY) {
     
-    if (keyCode == UP){
     noStroke();
     fill(255);
     ellipse(intX + 5, intY + 15, 30, 30);
     ellipse(intX - 10, intY + 15, 30, 30);
     ellipse(intX + 10, intY, 30, 30);
     ellipse(intX - 5, intY, 30, 30);
-    }
   }
-  
 
+  public void drawFence(int intX, int intY){
+    fill(71, 49, 45);
+    rect(intX, intY, 10, 80);
+    rect(intX, intY, width, 5);
+
+  }
 }
